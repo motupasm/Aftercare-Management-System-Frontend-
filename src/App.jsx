1ba2eedae1c158ded8ceaf52e459dc239e29eff0
unknown <motupams@gmail.com>
2026-03-01 04:27:51 +0200

    rename activities

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
 import ChooseUs from "./components/choose_us/ChooseUs";
 import VisionAndMission from "./components/vision_and_mission/VisionAndMission";
-import Activities from "./components/activities/activities";
+import Activities from "./components/activities/Activities";
 import Pricing from "./components/pricing/pricing";
 import Cta from "./components/cta/Cta";
 import Footer from "./components/footer/Footer";
